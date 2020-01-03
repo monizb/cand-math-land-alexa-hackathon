15cb734b131fc9cdb574adb303edff72f1f1c23b
Monish Basaniwal <46259712+monizb@users.noreply.github.com>
2020-01-03 12:57:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,3 @@
-/*
- Examples:
-
- Example 1:
- User: Alexa ask chocolate math
- Alexa: Welcome to chocolate Math Land. [sound of game start] ... How many math questions would you like to have?
- User: Five
- Alexa: Five questions that is. Question 1: Tommy has 5 ninja figures, Peter has 2 ninja figures. How many ninja figures do Tommy and Peter have together?
- User: Seven
- Alexa: You answered Seven. [sound of Cheers] ... You now have 10 chocolates. Question 2 ...
- ...
- ...
- Alexa: Good job. [sound of Game over] You have completed the chocolate Math game.  You have earned 50 chocolates. It was a pleasure playing with you. Come to visit Jolly Rancher Math Land soon. Goodbye!
-
-
- Example 2:
- User: Alexa open chocolate math
- Alexa: Welcome to chocolate Math Land. [sound of Game start] ... How many math questions would you like to have?
- User: Ten
- Alexa: Ten questions that is. Question 1: Tommy has 5 ninja figures, Peter has 2 ninja figures. How many ninja figures do Tommy and Peter have together?
- User: Seven
- Alexa: You answered Seven. [sound of beep] ... Then answer is wrong. You now have 0 chocolates. Question 2 ...
- ...
- ...
- Alexa: Good job. [sound of Game over] You have completed the chocolate Math game.  You have earned 50 chocolates. It was a pleasure playing with you. Come to visit Jolly Rancher Math Land soon. Goodbye!
- */
-
 var alexa = require('alexa-app');
 var IntentHandler = require('./intent_handler');
 
